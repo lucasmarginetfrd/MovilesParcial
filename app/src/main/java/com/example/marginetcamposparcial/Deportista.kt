489e87e2250d1590062483accbbf2a24bc9ff8c8
Lucas Alejandro Marginet Campos <lucasmarginetntick@gmail.com>
2024-09-29 23:32:30 -0300

    Version sin ingreso de datos

---
+++ app/src/main/java/com/example/marginetcamposparcial/Deportista.kt
@@ -0,0 +1,9 @@
+package com.example.marginetcamposparcial
+
+data class Deportista(
+    val id: Int,
+    val nombre: String,
+    val deporte: String,
+    val nacionalidad: String,
+    val actividad: String
+)
